95f184b4a067a6b378266b6b4c4314004738238b
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2015-04-19 17:55:30 +0200

    JMS enabled

--- src/main/java/de/zalando/social/shovel/ZalandoDemoApplication.java
+++ src/main/java/de/zalando/social/shovel/ZalandoDemoApplication.java
@@ -3,52 +3,54 @@ package de.zalando.social.shovel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.social.twitter.api.*;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
-import twitter4j.*;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan
 @EnableScheduling
+@EnableJms
 public class ZalandoDemoApplication {
 
     public static void main(String[] args) {
 
-        StatusListener listener = new StatusListener() {
-            public void onStatus(Status status) {
-                System.out.println(status.getUser().getName() + " : " + status.getText());
-            }
+//        StatusListener listener = new StatusListener() {
+//            public void onStatus(Status status) {
+//                System.out.println(status.getId() + " : " + status.getUser().getName() + " : " + status.getText());
+//            }
+//
+//            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+//                System.out.println(statusDeletionNotice.getStatusId());
+//            }
+//
+//            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+//            }
+//
+//            @Override
+//            public void onScrubGeo(long l, long l1) {
+//
+//            }
+//
+//            @Override
+//            public void onStallWarning(StallWarning stallWarning) {
+//
+//            }
+//
+//            public void onException(Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        };
+//        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+//        twitterStream.addListener(listener);
+//
+//        FilterQuery filterQuery = new FilterQuery();
+//        filterQuery.track(new String[]{"alibaba"});
+////        filterQuery.language(new String[]{"en"});
+//        twitterStream.filter(filterQuery);
+//        // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
+////        twitterStream.sample();
+        SpringApplication.run(ZalandoDemoApplication.class, args);
 
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-            }
-
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-            }
-
-            @Override
-            public void onScrubGeo(long l, long l1) {
-
-            }
-
-            @Override
-            public void onStallWarning(StallWarning stallWarning) {
-
-            }
-
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-        };
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-        twitterStream.addListener(listener);
-
-        FilterQuery filterQuery = new FilterQuery();
-        filterQuery.track(new String[]{"#Zalando"});
-        twitterStream.filter(filterQuery);
-        // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
-//        twitterStream.sample();
     }
 }
--- src/main/java/de/zalando/social/shovel/social/ScheduledTasks.java
+++ src/main/java/de/zalando/social/shovel/social/ScheduledTasks.java
@@ -1,6 +1,9 @@
 package de.zalando.social.shovel.social;
 
+import de.zalando.social.shovel.messaging.Message;
 import de.zalando.social.shovel.websocket.SimpleHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -13,17 +16,23 @@ import java.io.IOException;
  */
 @Component
 public class ScheduledTasks {
-//    @Scheduled(fixedRate = 15000)
+
+    @Autowired
+    private JmsTemplate template;
+
+
+        @Scheduled(fixedRate = 15000)
     public void doIt() {
-        System.out.println("Number of sessions: " + SimpleHandler.sessions.size());
-        for (WebSocketSession s : SimpleHandler.sessions) {
-            try {
-                s.sendMessage(new TextMessage("Hello"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("Hi man, i am from scheduler.");
+//        System.out.println("Number of sessions: " + SimpleHandler.sessions.size());
+//        for (WebSocketSession s : SimpleHandler.sessions) {
+//            try {
+//                s.sendMessage(new TextMessage("Hello"));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        System.out.println("Hi man, i am from scheduler.");
+            template.convertAndSend("dest1",new Message("hi"));
 
     }
 }
