77444e8fb51f8a058fe0c6e7741e8b49f642e329
soroosh sarabadani <soroosh.sarabadani@zalando.de>
2015-04-20 15:50:32 +0200

    message used line commented to disappear error

--- client-api/src/main/java/de/zalando/social/shovel/web/ZalandoDemoApplication.java
+++ client-api/src/main/java/de/zalando/social/shovel/web/ZalandoDemoApplication.java
@@ -4,18 +4,18 @@ import de.zalando.social.shovel.service.configuration.ServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 
 @SpringBootApplication
-@ComponentScan({"de.zalando.social.shovel.web.controller","de.zalando.social.shovel.web.configuration"})
+@ComponentScan({"de.zalando.social.shovel.web.controller", "de.zalando.social.shovel.web.configuration"})
 @Import({ServiceConfiguration.class})
 public class ZalandoDemoApplication {
 
     public static void main(String[] args) {
+
+
 
         SpringApplication.run(ZalandoDemoApplication.class, args);
 
