939850e1bd9a7350a45e19802464669226cd1c17
soroosh sarabadani <soroosh.sarabadani@zalando.de>
2015-04-20 15:49:02 +0200

    Message brushed more

--- service/src/main/java/de/zalando/social/shovel/service/messaging/Message.java
+++ service/src/main/java/de/zalando/social/shovel/service/messaging/Message.java
@@ -1,23 +1,57 @@
 package de.zalando.social.shovel.service.messaging;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
  * Created by SOROOSH on 4/19/15.
  */
 public class Message implements Serializable {
-    private final String topic;
-    private final String source;
-    private final String content;
-    private final String language;
-    private final UserInfo userInfo;
-    private final String provider;
-    //TODO: Maybe a builder pattern is better here.
-    public Message(String topic, String source, String content, String language, UserInfo userInfo, String provider, Date postedDate) {
+    public static class MessageBuilder {
+        private final Message msg;
 
-        this.topic = topic;
-        this.source = source;
+        public MessageBuilder(String content, String provider,String... topics) {
+            this.msg = new Message(content, provider,topics);
+        }
+
+        public MessageBuilder withLang(String lang) {
+            this.msg.language = lang;
+            return this;
+        }
+
+        public MessageBuilder withUserInfo(String userId, String name, String lastName, String imageUrl) {
+            UserInfo userInfo = new UserInfo(userId, name, lastName, imageUrl);
+            this.msg.userInfo = userInfo;
+            return this;
+
+        }
+
+        public MessageBuilder on(Date date) {
+            this.msg.postedDate = date;
+            return this;
+        }
+
+        public Message build() {
+            return this.msg;
+        }
+    }
+
+    private final String[] topics;
+    private final String provider;
+    private final String content;
+    private String language;
+    private UserInfo userInfo;
+    private Date postedDate;
+
+    private Message(String content, String provider,String... topics) {
+        this.topics = topics;
+        this.content = content;
+        this.provider = provider;
+    }
+
+    public Message(String content, String language, UserInfo userInfo, String provider, Date postedDate,String... topics) {
+        this.topics = topics;
         this.content = content;
         this.language = language;
         this.userInfo = userInfo;
@@ -45,17 +79,19 @@ public class Message implements Serializable {
         return content;
     }
 
-    public String getSource() {
-        return source;
+    public String[] getTopics() {
+        return topics;
     }
 
-    public String getTopic() {
-        return topic;
+    @Override
+    public String toString() {
+        return "Message{" +
+                "topics=" + Arrays.toString(topics) +
+                ", provider='" + provider + '\'' +
+                ", content='" + content + '\'' +
+                ", language='" + language + '\'' +
+                ", userInfo=" + userInfo +
+                ", postedDate=" + postedDate +
+                '}';
     }
-
-    private final Date postedDate;
-
-
-
-
 }
