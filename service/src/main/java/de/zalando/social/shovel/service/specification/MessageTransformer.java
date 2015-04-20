a98e6bd0482b0895f377379c6dfccf822a86f779
soroosh sarabadani <soroosh.sarabadani@zalando.de>
2015-04-20 15:24:04 +0200

    transform signature changed to support topics

--- service/src/main/java/de/zalando/social/shovel/service/specification/MessageTransformer.java
+++ service/src/main/java/de/zalando/social/shovel/service/specification/MessageTransformer.java
@@ -6,5 +6,5 @@ import de.zalando.social.shovel.service.messaging.Message;
  * Created by soroosh on 4/20/15.
  */
 public interface MessageTransformer<T> {
-    Message transform(T t);
+    Message transform(T t,String... topics);
 }
