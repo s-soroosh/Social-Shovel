a8645e06990709a19036187ed4c550e767c9c285
soroosh sarabadani <soroosh.sarabadani@zalando.de>
2015-04-20 15:08:46 +0200

    Message Transformer spec added

---
+++ service/src/main/java/de/zalando/social/shovel/service/specification/MessageTransformer.java
@@ -0,0 +1,10 @@
+package de.zalando.social.shovel.service.specification;
+
+import de.zalando.social.shovel.service.messaging.Message;
+
+/**
+ * Created by soroosh on 4/20/15.
+ */
+public interface MessageTransformer<T> {
+    Message transform(T t);
+}
