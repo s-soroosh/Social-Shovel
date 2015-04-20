732a98d92665b8025ef2ef8dc8cc5544d96df8d0
dmytro.zharkov@gmail.com <dzharkov@zalando-08184.corp.ad.zalando.net>
2015-04-20 17:43:27 +0200

    Pie charts

--- frontend/app/scripts/controllers/about.js
+++ frontend/app/scripts/controllers/about.js
@@ -14,5 +14,22 @@ angular.module('zssApp')
       'AngularJS',
       'Karma'
     ];
-    d3.select("body").append("p").text("New paragraph!");
+      var dataset = [
+        { label: 'Abulia', count: 10 },
+        { label: 'Betelgeuse', count: 20 },
+        { label: 'Cantaloupe', count: 30 },
+        { label: 'Dijkstra', count: 40 }
+      ];
+      var width = 360;
+      var height = 360;
+      var radius = Math.min(width, height) / 2;
+      var color = d3.scale.category20b();
+
+      var svg = d3.select('.dataHolder')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
   });
