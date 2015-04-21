6dd91f838d2f8ded8a147ae5731d518c035c2c09
dmytro.zharkov@gmail.com <dzharkov@zalando-08184.corp.ad.zalando.net>
2015-04-21 11:18:42 +0200

    Frontend view change

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -9,9 +9,36 @@
  */
 angular.module('zssApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+      var dataset = [
+        { label: 'Abulia', count: 10 },
+        { label: 'Betelgeuse', count: 20 },
+        { label: 'Cantaloupe', count: 30 },
+        { label: 'Dijkstra', count: 40 }
+      ];
+      var width = 360;
+      var height = 360;
+      var radius = Math.min(width, height) / 2;
+      var color = d3.scale.category20b();
+
+      var svg = d3.select('#genralData')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+      var arc = d3.svg.arc()
+          .outerRadius(radius);
+      var pie = d3.layout.pie()
+          .value(function(d) { return d.count; })
+          .sort(null);
+      var path = svg.selectAll('path')
+          .data(pie(dataset))
+          .enter()
+          .append('path')
+          .attr('d', arc)
+          .attr('fill', function(d, i) {
+            return color(d.data.label);
+          });
+
+    });
