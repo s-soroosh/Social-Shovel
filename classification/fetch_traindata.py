2d235a2bedfd00e6dad0010d036ec94ab70787f3
Frank Honza <frank.honza@zalando.de>
2015-04-22 17:05:56 +0200

    some cleanup

--- classification/fetch_traindata.py
+++ classification/fetch_traindata.py
@@ -1,5 +1,5 @@
-def get_data():
-    source = file("train_data.txt", "rb")
+def get_data(filename):
+    source = file(filename, "rb")
     data = []
     labels = []
     for l in source:
@@ -10,14 +10,3 @@ def get_data():
             labels.append(label)
     return data, labels
 
-def get_fashion_data():
-    source = file("train_fashion_data.txt", "rb")
-    data = []
-    labels = []
-    for l in source:
-        entry = l.split("%%%")
-        if len(entry) == 2:
-            data.append(entry[0])
-            label = int(entry[1].strip())
-            labels.append(label)
-    return data, labels            
